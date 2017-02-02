9167f89a6cd828306becf20951e49e753c0668be
Abokor <abokor_88@hotmail.com>
2017-02-02 20:59:59 +0300

    Finished Styling the Login Page

--- index.php
+++ index.php
@@ -4,21 +4,29 @@
   <meta charset="utf-8">
   <title>Accounting WebApp</title>
   <link rel="stylesheet" href="style/main.css">
-  <script src="script/script.js"></script>
+  <link rel="stylesheet" href="style/bootstrap.min.css">
+
 </head>
 <body>
   <!-- The rest of the content -->
   <div class = "container">
     <div class="wrapper">
-  		<form action="" method="post" name="Login_Form" class="form-signin">
-  		  <h3 class="form-signin-heading">Welcome Back! Please Sign In</h3>
-  			<hr class="colorgraph"><br>
+      <div> <center><img src="images/logo.png" alt="AKIHA_TECH Logo" /></center> </div><br />
+      <div class="row" style="margin-top:20px">
+        <div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
+      		<form action="" method="post" name="Login_Form" role="form">
+          <fieldset>
+      		  <h3 class="form-signin-heading">Please Sign In</h3>
+      			<hr class="colorgraph"><br>
 
-  			<input type="text" class="form-control" name="Username" placeholder="Username" required="" autofocus="" />
-  			<input type="password" class="form-control" name="Password" placeholder="Password" required=""/>
+      			<input type="text" class="form-control input-lg" name="Username" placeholder="Username" required="" autofocus="" />
+      			<input type="password" class="form-control input-lg" name="Password" placeholder="Password" required=""/>
 
-  			<button class="btn btn-lg btn-primary btn-block"  name="Submit" value="Login" type="Submit">Login</button>
-  		</form>
+      			<button class="btn btn-lg btn-success btn-block"  name="Submit" value="Login" type="Submit">Login</button>
+          </fieldset>
+      		</form>
+        </div>
+      </div>
     </div>
   </div>
 
